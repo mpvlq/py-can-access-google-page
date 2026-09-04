17a147e7b5fc6bffefa506fcfe7077e6b62f48f2
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-09-04 02:24:55 +0200

    solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,13 @@
 # write your code here
-import pytest
-
 from unittest import mock
 
 from app.main import can_access_google_page
 
-def test_if_url_is_valid():
-    url = 'https://google.com'
+
+def test_if_url_is_valid() -> None:
+    url = "https://google.com"
     mocked_valid_google_url = mock.MagicMock()
     mocked_has_internet_connection = mock.MagicMock()
-    assert can_access_google_page(url) == "Not accessible"
+    can_access_google_page(url)
+    mocked_valid_google_url.assert_called_once()
+    mocked_has_internet_connection.assert_called_once()
